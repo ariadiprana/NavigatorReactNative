3544f2ff66854d4f6f7d338f02b8eaf2ad8b076f
ariadiprana <ari.adiprana@gmail.com>
2016-11-09 09:37:49 +0700

    release

--- components/PageOne.js
+++ components/PageOne.js
@@ -11,7 +11,7 @@ class PageOne extends React.Component {
     return (
       <View style={{padding:60}}>
         <Text>Greetings From Page 1!</Text>
-        <TouchableOpacity onPress={}>
+        <TouchableOpacity>
           <View>
             <Text>Go to page 2</Text>
           </View>
