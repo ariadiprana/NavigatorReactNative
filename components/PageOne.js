13cb6e513f172e616b794b4d4fcfb165e0b96e7a
ariadiprana <ari.adiprana@gmail.com>
2016-11-09 09:42:23 +0700

    release

--- components/PageOne.js
+++ components/PageOne.js
@@ -6,14 +6,25 @@ import {
 } from 'react-native'
 
 
+
 class PageOne extends React.Component {
+  constructor(props){
+    super(props)
+    this._handlePress = this._handlePress.bind(this)
+  }
+
+  _handlePress(){
+    this.props.navigator.push({id:2})
+  }
+
+
   render(){
     return (
       <View style={{padding:60}}>
         <Text>Greetings From Page 1!</Text>
-        <TouchableOpacity>
+        <TouchableOpacity onPress={this._handlePress.bind(this)}>
           <View style={{paddingVertical:10,paddingHorizontal:20,backgroundColor:'black'}}>
-            <Text>Go to page 2</Text>
+            <Text style={{color:'white'}}>Go to page 2</Text>
           </View>
         </TouchableOpacity>
       </View>
